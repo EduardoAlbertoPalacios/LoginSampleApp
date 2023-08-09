63570a285c465c57df083c5aa06495afe191e9a2
eduardopalacios <inmortalbeto@hotmail.es>
2023-08-08 21:59:16 -0600

    [MD-123][UserStory] JavDoc in app login module.

--- app/src/main/java/com/example/loginsampleapp/ui/component/alertDialog/AlertDialogType.kt
+++ app/src/main/java/com/example/loginsampleapp/ui/component/alertDialog/AlertDialogType.kt
@@ -1,5 +1,8 @@
 package com.example.loginsampleapp.ui.component.alertDialog
 
+/**
+ * Represents the types of alerts that can be used in [CustomDialog]
+ */
 enum class AlertDialogType {
     SUCCESS,
     ERROR,
--- app/src/main/java/com/example/loginsampleapp/ui/component/progressBar/ProgressBarLogoType.kt
+++ app/src/main/java/com/example/loginsampleapp/ui/component/progressBar/ProgressBarLogoType.kt
@@ -1,5 +1,8 @@
 package com.example.loginsampleapp.ui.component.progressBar
 
+/**
+ * Define the animation type that can be used in [CustomProgressBar] as a composition.
+ */
 enum class ProgressBarLogoType {
     SPIRAL,
     NONE,
